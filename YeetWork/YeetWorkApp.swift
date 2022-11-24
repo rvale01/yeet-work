0fffc424ba818cb0d24e475303a7a27f7a82e82b
rvale01 <rvale09@gmail.com>
2022-11-24 14:45:49 +0000

    Initial Commit

---
+++ YeetWork/YeetWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeetWorkApp.swift
+//  YeetWork
+//
+//  Created by Valentina Ronchi on 24/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct YeetWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
